ada7a24bd00b3ea8d7f756a7e155f72425c6f4ee
Baba <babasaheb.ladhane@gmail.com>
2021-09-11 23:32:00 +1000

    init code src and public folder

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import './App.css';
+import ButtonAppBar from './components/Header'
+import TitlebarGridList from './components/TreeList';
+import MyButton from './components/Footer'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ButtonAppBar />
+        <TitlebarGridList />
+        <MyButton/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/treeListData.js
@@ -0,0 +1,63 @@
+const treeListData = [
+    {
+    img: 'images/apple.jpeg',
+    title: 'Apple',
+    },
+    {
+    img: 'images/banyan.jpeg',
+    title: 'Banyan tree',
+    featured: true,
+    },
+    {
+      img: 'images/cherry.jpeg',
+      title: 'Cherry',
+    },
+    {
+      img: 'images/citrus.jpeg',
+      title: 'Citrus',
+    },
+    {
+      img: 'images/fig.jpeg',
+      title: 'Fig',
+    },
+    {
+      img: 'images/guava.jpeg',
+      title: 'Guava',
+      featured: true,
+    },
+    {
+      img: 'images/maple.jpeg',
+      title: 'Maple',
+    },
+    {
+      img: 'images/orange.jpeg',
+      title: 'Orange',
+    },
+    {
+      img: 'images/Lemon.jpeg',
+      title: 'Lemon',
+    },
+    {
+      img: 'images/lillypilly.jpeg',
+      title: 'Lillypilly',
+      featured: true,
+    },
+    {
+      img: 'images/pomegranate.jpeg',
+      title: 'Pomegranate',
+    },
+    {
+      img: 'images/tamarind.jpeg',
+      title: 'Tamarind',
+    },
+    {
+      img: 'images/berry.jpeg',
+      title: 'Berry',
+    },
+    {
+      img: 'images/apple1.jpeg',
+      title: 'Apple popular',
+    },
+  ];
+  
+  export default treeListData;
